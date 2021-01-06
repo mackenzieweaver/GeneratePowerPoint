3be9c659297f76ed52783fe3776c7e78b10fa213
Mack <macknz7@gmail.com>
2021-01-05 19:27:36 -0800

    Initial Setup
    Favicon
    Title - default text
    Body - default text
    Buttons
    
    method setup

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,121 @@ namespace GeneratePowerPoint
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.generateSlide = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.titleBox = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.titleBox, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.richTextBox1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.generateSlide);
+            this.flowLayoutPanel1.Controls.Add(this.closeButton);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 408);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(794, 39);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // generateSlide
+            // 
+            this.generateSlide.AutoSize = true;
+            this.generateSlide.Location = new System.Drawing.Point(3, 3);
+            this.generateSlide.Name = "generateSlide";
+            this.generateSlide.Size = new System.Drawing.Size(96, 23);
+            this.generateSlide.TabIndex = 0;
+            this.generateSlide.Text = "Generate a Slide";
+            this.generateSlide.UseVisualStyleBackColor = true;
+            this.generateSlide.Click += new System.EventHandler(this.generateSlide_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.AutoSize = true;
+            this.closeButton.Location = new System.Drawing.Point(105, 3);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 3;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // titleBox
+            // 
+            this.titleBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.titleBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleBox.ForeColor = System.Drawing.Color.Black;
+            this.titleBox.Location = new System.Drawing.Point(3, 3);
+            this.titleBox.Name = "titleBox";
+            this.titleBox.Size = new System.Drawing.Size(794, 49);
+            this.titleBox.TabIndex = 3;
+            this.titleBox.Text = "Click to add title";
+            this.titleBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.titleBox.Click += new System.EventHandler(this.titleBox_Click);
+            this.titleBox.TextChanged += new System.EventHandler(this.titleBox_TextChanged);
+            this.titleBox.Leave += new System.EventHandler(this.titleBox_Leave);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.Black;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 93);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(794, 129);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "Click to add text";
+            this.richTextBox1.Click += new System.EventHandler(this.richTextBox1_Click);
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            this.richTextBox1.Leave += new System.EventHandler(this.richTextBox1_Leave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Generate Power Point";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button generateSlide;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.TextBox titleBox;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,61 @@ namespace GeneratePowerPoint
         {
             InitializeComponent();
         }
+        
+        // 1 TODO: export a file in .pptx
+        private void generateSlide_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        // 2 TODO: request new pictures
+        private void titleBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void titleBox_Click(object sender, EventArgs e)
+        {
+            if (titleBox.Text == "Click to add title")
+            {
+                titleBox.Text = "";
+            }
+        }
+
+        private void titleBox_Leave(object sender, EventArgs e)
+        {
+            if (titleBox.Text == "")
+            {
+                titleBox.Text = "Click to add title";
+            }
+        }
+
+        // 4 TODO: if bold text request new pictures
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text == "Click to add text")
+            {
+                richTextBox1.Text = "";
+            }
+        }
+
+        private void richTextBox1_Leave(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text == "")
+            {
+                richTextBox1.Text = "Click to add text";
+            }
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            Close();
+        }
     }
 }
